9ef107e1f967686a60502b542e1c812e1b1765f7
Roger D. Peng [amelia] <rdpeng@gmail.com>
2013-11-06 10:08:17 -0500

    Add unit test for fastlm

---
+++ Homework1/tests/test-fastlm01.R
@@ -0,0 +1,17 @@
+library(Homework1)
+
+set.seed(2)
+## Generate predictor matrix
+n <- 1000
+p <- 50
+X <- cbind(1, matrix(rnorm(n * (p - 1)), n, p - 1))
+
+## Coefficents
+b <- rnorm(p)
+
+## Response
+y <- X %*% b + rnorm(n)
+
+fit <- fastlm(X, y)
+print(fit$coefficients)
+str(fit$vcov)
