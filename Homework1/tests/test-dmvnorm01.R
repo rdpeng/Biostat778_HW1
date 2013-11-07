38f38154b85b22cd94be845783337d9b93bbec43
Roger D. Peng [amelia] <rdpeng@gmail.com>
2013-11-07 10:33:31 -0500

    Unit test for dmvnorm

---
+++ Homework1/tests/test-dmvnorm01.R
@@ -0,0 +1,17 @@
+library(Homework1)
+
+n <- 10
+n2 <- n^2
+xg <- seq(0, 1, length = n)
+yg <- xg
+g <- data.matrix(expand.grid(xg, yg))
+D <- as.matrix(dist(g))
+phi <- 5
+
+S <- exp(-phi * D)
+mu <- rep(0, n2)
+set.seed(1)
+x <- matrix(rnorm(n2), byrow = TRUE, ncol = n2)
+
+y <- dmvnorm(x, mu, S, log = TRUE)
+print(y, digits = 10)
