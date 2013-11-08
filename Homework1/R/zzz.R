10d4464313d37e993707a176b63d44db170f43e8
Roger D. Peng [amelia] <rdpeng@gmail.com>
2013-11-08 13:03:50 -0500

    Don't print startup message

--- Homework1/R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onAttach <- function(lib, pkg) {
-        dcf <- read.dcf(file.path(lib, pkg, "DESCRIPTION"))
-        msg <- gettextf("%s: %s (version %s)", dcf[, "Package"], dcf[, "Title"],
-                        as.character(dcf[, "Version"]))
-        packageStartupMessage(paste(strwrap(msg), collapse = "\n"))
-}
