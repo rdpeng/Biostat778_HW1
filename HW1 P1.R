a07d6273f69e835c4ce88be0c626e599c849e3dd
lli48 <lli48@jhu.edu>
2013-11-08 14:12:34 -0500

    fasterlm

--- HW1 P1.R
+++ HW1 P1.R
@@ -1,38 +1,27 @@
 #define function
-fastlm<-function(X, y, na.rm = FALSE) 
-  {
-    n<-length(y)
-    p<-ncol(X)
+fastlm<-function(X, y, na.rm = FALSE) {
+        n<-length(y)
+        p<-ncol(X)
+    #check na.rm
+        if (na.rm==TRUE){
+                Z=cbind(X,y)
+                X=X[complete.cases(Z),]
+                y=as.matrix(y[complete.cases(Z)])
+        }
     
-    # calculating transpose(X)%*%X
-    A<-crossprod(X,X) 
-    C<-crossprod(X,y)
+    #calculating transpose(X)%*%X
+        A<-crossprod(X)
+        C<-crossprod(X,y)
     
     #cholesky decomposition
-    Q<-chol(A)
+        Q<-chol(A)
    
     #solve betahat
-    temp1<-forwardsolve(t(Q),C) 
-    betahat<-backsolve(Q,temp1) 
+        temp1<-forwardsolve(t(Q),C) 
+        betahat<-backsolve(Q,temp1) 
     
     #calculate covirance of beta
-    cov_beta<-chol2inv(Q)*as.numeric(crossprod(y-X%*%betahat)/(n-p))
+        cov_beta<-chol2inv(Q)*as.numeric(crossprod(y-X%*%betahat)/(n-p))
     
-    return(list(coeffients=betahat,vcov=cov_beta))
-  }
-set.seed(2)
-## Generate predictor matrix
-n <- 100000
-p <- 500
-X <- cbind(1, matrix(rnorm(n * (p - 1)), n, p - 1))
-
-## Coefficents
-b <- rnorm(p)
-
-## Response
-y <- X %*% b + rnorm(n)
-
-
-fit <- fastlm(X, y)
-str(fit)
-system.time(fit<-fastlm(X, y))
+        return(list(coeffients=betahat,vcov=cov_beta))
+}
