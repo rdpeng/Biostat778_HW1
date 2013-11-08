244c77a3df9af85a0f5363fb9285be2b3ea12543
Roger D. Peng [amelia] <rdpeng@gmail.com>
2013-11-08 10:06:54 -0500

    dmvnorm unit test 2

---
+++ Homework1/tests/test-dmvnorm02.R
@@ -0,0 +1,11 @@
+## Check `log = FALSE'
+
+library(Homework1)
+
+mu <- rep(0, 10)
+S <- diag(2, 10)
+x <- matrix(rep(0, 20), 2, 10)
+y <- dmvnorm(x, mu, S, log = FALSE)
+print(y, digits = 10)
+y <- dmvnorm(x, mu, S, log = TRUE)
+print(y, digits = 10)
