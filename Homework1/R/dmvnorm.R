b2b0dfd8ad8438a2a7607593f5d6516d73692f6e
Yuxin(Daisy) Zhu <daisy.zhu.yx@gmail.com>
2013-11-07 22:35:54 -0500

    Homework1 submission

---
+++ Homework1/R/dmvnorm.R
@@ -0,0 +1,12 @@
+dmvnorm<-function(x, mu, S, log=TRUE)
+{
+      k<-length(mu)
+      cs<-tryCatch({chol(S)}, error=function(cond)
+            {stop("S is not positive definite")})
+      
+      ## calculate log density
+      a<-forwardsolve(t(cs),t(x)-mu)
+      logf<--log(2*pi)*k/2-sum(log(diag(cs)))-colSums(a^2)/2
+
+      return(logf^log*exp(logf)^(1-log))
+}
