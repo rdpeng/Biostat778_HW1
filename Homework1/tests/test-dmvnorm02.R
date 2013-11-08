e91486f0dba743d75aedad1a327c1b549ca50fe2
Roger D. Peng [amelia] <rdpeng@gmail.com>
2013-11-08 10:17:16 -0500

    Update test 2 for dmvnorm

--- Homework1/tests/test-dmvnorm02.R
+++ Homework1/tests/test-dmvnorm02.R
@@ -4,7 +4,7 @@ library(Homework1)
 
 mu <- rep(0, 10)
 S <- diag(2, 10)
-x <- matrix(rep(0, 20), 2, 10)
+x <- matrix(rbind(rep(0, 10), rep(1, 10)), 2, 10)
 y <- dmvnorm(x, mu, S, log = FALSE)
 print(y, digits = 15)
 y <- dmvnorm(x, mu, S, log = TRUE)
