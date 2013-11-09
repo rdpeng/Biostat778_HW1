ec58307b79750dd7be9760b69918aa6312f778ac
Yuxin(Daisy) Zhu <daisy.zhu.yx@gmail.com>
2013-11-08 21:15:26 -0500

    changes ancillary to "improve fastlm speed"

--- Homework1.Rcheck/00_pkg_src/Homework1/R/dmvnorm.R
+++ Homework1.Rcheck/00_pkg_src/Homework1/R/dmvnorm.R
@@ -1,14 +1,13 @@
 dmvnorm<-function(x, mu, S, log=TRUE)
 {
-      k<-length(mu)
       cs<-tryCatch({chol(S)}, error=function(cond)
             {stop("S is not positive definite")})
       
       ## calculate log density
       a<-forwardsolve(t(cs),t(x)-mu)
-      logf<--log(2*pi)*k/2-sum(log(diag(cs)))-colSums(a^2)/2
+      logf<--log(2*pi)*length(mu)/2-sum(log(diag(cs)))-colSums(a*a)/2
 
-      if(log) result<-logf
+      if(log==T) result<-logf
       else result<-exp(logf)
       result
 }
