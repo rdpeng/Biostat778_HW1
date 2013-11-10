4fbdca8af3bd62a848d36b2cf0c314cb35abf9cf
zhicheng ji <zhichengji@zhichengs-MacBook-Pro.local>
2013-11-10 10:41:18 -0500

    Minor changes to dmvnorm

--- Homework1/R/dmvnorm.R
+++ Homework1/R/dmvnorm.R
@@ -1,11 +1,10 @@
 ###Fast Multivariate Normal Density
 dmvnorm <- function(x, mu, S, log=TRUE) {
+      cholS <- tryCatch(chol(S),error=function(cond) {stop("S is not positive definite",call. = F)})
       if (is.vector(x))
             x <- as.matrix(t(x))
-      cholS <- tryCatch(chol(S),error=function(cond) {stop("S is not positive definite",call. = F)})
-      k <- ncol(x)
       res <- sapply(1:nrow(x), function(i) {
-            -(k/2)*log(2*pi) - sum(log(diag(cholS))) - .5*sum(crossprod(forwardsolve(t(cholS),x[i,]-mu)))
+            -(ncol(x)/2)*log(2*pi) - sum(log(diag(cholS))) - .5*sum(crossprod(forwardsolve(t(cholS),x[i,]-mu)))
       })
       if (log == TRUE) {
             res
