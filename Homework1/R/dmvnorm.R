1d09fecf487cf4f6c403267df6a927dfc3d3d635
YuanHe16 <grasslandhy@gmail.com>
2013-11-12 14:04:20 -0500

    r-package

--- Homework1/R/dmvnorm.R
+++ Homework1/R/dmvnorm.R
@@ -1,26 +1,18 @@
 dmvnorm <-
-function(x, mu, S, log=TRUE)
+function (x, mu, S, log = TRUE) 
 {
-  # check if S is positive definite
-  l = tryCatch(chol(S),error = function(e){message ("S is not positive definite")})
-  
-  # log(det(S))
-  logdS = 2*sum(log(diag(l)))
-  
-  # (x-mu)
-  n = nrow(x)
-  Mx = x - rep(1,n)%*%t(mu)
-  
-  # Y = backsolve(l,Mx)
-  Y = forwardsolve(t(l),t(Mx))
-  Mx3 = diag(crossprod(Y))
-  
-  k = length(mu)
-  fx = -(k/2)*log(2*pi)-(1/2)*logdS-(1/2)*Mx3
-  
-  if (log == FALSE)
-    fx = exp(fx)
-  else fx= fx
-  
-  return(fx)
+    l = tryCatch(chol(S), error = function(e) {
+        message("S is not positive definite")
+    })
+    logdS = 2 * sum(log(diag(l)))
+    n = nrow(x)
+    Mx = x - rep(1, n) %*% t(mu)
+    Y = forwardsolve(t(l), t(Mx))
+    Mx3 = diag(crossprod(Y))
+    k = length(mu)
+    fx = -(k/2) * log(2 * pi) - (1/2) * logdS - (1/2) * Mx3
+    if (log == FALSE) 
+        fx = exp(fx)
+    else fx = fx
+    return(fx)
 }
--- Homework1/R/fastlm.R
+++ Homework1/R/fastlm.R
@@ -1,25 +1,18 @@
 fastlm <-
-function(x,y,na.rm=FALSE)
+function (x, y, na.rm = FALSE) 
 {
-  # get rid of NAs
-  if (na.rm == TRUE)
-  {
-    r = cbind(x,y)
-    x = x[complete.cases(r),]
-    y = as.matrix(y[complete.cases(r)])
-  }
-  
-  #coefficient
-  A = crossprod(x)
-  B = crossprod(x,y)
-  l = chol(A)
-  beta = backsolve(l, forwardsolve(t(l),B))
-
-  #covaviance
-  k = ncol(x)
-  n = nrow(y)
-  sigmaS = (crossprod(y,y) - crossprod(beta,B))/(n-k)
-  covBeta = as.numeric(sigmaS)* chol2inv(l)
-
-  list(beta,covBeta)
+    if (na.rm == TRUE) {
+        r = cbind(x, y)
+        x = x[complete.cases(r), ]
+        y = as.matrix(y[complete.cases(r)])
+    }
+    A = crossprod(x)
+    B = crossprod(x, y)
+    l = chol(A)
+    beta = backsolve(l, forwardsolve(t(l), B))
+    k = ncol(x)
+    n = nrow(y)
+    sigmaS = (crossprod(y, y) - crossprod(beta, B))/(n - k)
+    covBeta = as.numeric(sigmaS) * chol2inv(l)
+    list(beta, covBeta)
 }
