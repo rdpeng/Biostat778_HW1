2dce99c33f1406343334b92faa9359645a0512be
YuanHe16 <grasslandhy@gmail.com>
2013-11-12 14:13:39 -0500

    R-package

--- Homework1/R/dmvnorm.R
+++ Homework1/R/dmvnorm.R
@@ -1,18 +1,25 @@
-dmvnorm <-
-function (x, mu, S, log = TRUE) 
+dmvnorm <- function(x, mu, S, log=TRUE)
 {
-    l = tryCatch(chol(S), error = function(e) {
-        message("S is not positive definite")
-    })
-    logdS = 2 * sum(log(diag(l)))
-    n = nrow(x)
-    Mx = x - rep(1, n) %*% t(mu)
-    Y = forwardsolve(t(l), t(Mx))
-    Mx3 = diag(crossprod(Y))
-    k = length(mu)
-    fx = -(k/2) * log(2 * pi) - (1/2) * logdS - (1/2) * Mx3
-    if (log == FALSE) 
-        fx = exp(fx)
-    else fx = fx
-    return(fx)
+  # check if S is positive definite
+  l = tryCatch(chol(S),error = function(e){message ("S is not positive definite")})
+  
+  # log(det(S))
+  logdS = 2*sum(log(diag(l)))
+  
+  # (x-mu)
+  n = nrow(x)
+  Mx = x - rep(1,n)%*%t(mu)
+  
+  # Y = backsolve(l,Mx)
+  Y = forwardsolve(t(l),t(Mx))
+  Mx3 = diag(crossprod(Y))
+  
+  k = length(mu)
+  fx = -(k/2)*log(2*pi)-(1/2)*logdS-(1/2)*Mx3
+  
+  if (log == FALSE)
+    fx = exp(fx)
+  else fx= fx
+  
+  return(fx)
 }
