e11cf388b9712a3d7d371e14163baf2f61fbda15
Oliver Chen <olivery.chen@yahoo.com>
2013-11-13 18:19:49 -0500

    Update dmvnorm.R

--- Homework1/R/dmvnorm.R
+++ Homework1/R/dmvnorm.R
@@ -2,12 +2,12 @@ dmvnorm <- function(x, mu, S, log = TRUE) {
       
       
       #if (is.vector(mu)==F)  mu<-as.vector(mu)
-      if (is.matrix(x)==F)  x<-as.matrix(x)
+      if (is.matrix(x)==F)  x<-t(as.matrix(x))
       
       if (min(eigen(S)$values)>0){
             k <- length(mu)
         rooti <- backsolve(chol(S),diag(k))
-        quads <- colSums((crossprod(rooti,(x-mu)))^2)
+        quads <- colSums((crossprod(rooti,(t(x)-mu)))^2)
         y=exp(-(k/2)*log(2*pi) + sum(log(diag(rooti))) - .5*quads)
 
         if(log==TRUE) return (log(y))
