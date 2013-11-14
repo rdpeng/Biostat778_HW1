12abc3562d91d49b41a31ad8ca4fa52c1503ca1d
Jerryppa <09fudansqy@gmail.com>
2013-11-13 20:24:20 -0500

    Final

--- Homework1/R/dmvnorm.R
+++ Homework1/R/dmvnorm.R
@@ -1,15 +1,14 @@
 dmvnorm <-
 function(x, mu, S, log = TRUE) {
-  ##Check if S is positive definite
   R=tryCatch({chol(S)},
              error=function(e){
                message("S is not positive definite")
              })
   
-  logs=2*sum(log(diag(R)))
   y=x-rep(1,nrow(x))%*%t(mu)
   z=forwardsolve(t(R),t(y))
   cov=diag(crossprod(z))
+  logs=sum(log(diag(R)))+sum(log(diag(t(R))))
   final=(-length(mu)/2)*log(2*pi)-(1/2)*logs-(1/2)*cov
   if(log==TRUE){
     final=final
